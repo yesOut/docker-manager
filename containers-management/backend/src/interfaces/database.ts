08596b9b7a0e3f897e1affc36747aeb7c9165741
yes-in <jeyesn350@gmail.com>
2025-04-25 14:49:22 +0100

    add/connexion_architecture

--- containers-management/backend/src/interfaces/database.ts
+++ containers-management/backend/src/interfaces/database.ts
@@ -1,13 +1,21 @@
+import {IEntity} from "@/connexions/mongo.db";
+
 export interface IDatabase {
     connect(): Promise<void>;
+
     disconnect(): Promise<void>;
-    getRepository<T>(entity: new () => T): IRepository<T>;
+
+    getRepository<T extends IEntity>(entity: new () => T): IRepository<T>;
 }
 
 export interface IRepository<T> {
     findById(id: string): Promise<T | null>;
+
     findAll(): Promise<T[]>;
+
     create(entity: Omit<T, 'id'>): Promise<T>;
+
     update(id: string, updates: Partial<T>): Promise<T | null>;
+
     delete(id: string): Promise<boolean>;
 }
