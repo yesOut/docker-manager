268272a1caf81a93266051b2fe2a43723339fc4e
Youssef <limemyoussef@bizerte.r-iset.tn>
2025-05-09 12:54:07 +0100

    linking the register to the backend

--- containers-management/frontend/src/components/Login.tsx
+++ containers-management/frontend/src/components/Login.tsx
@@ -30,7 +30,7 @@ export default function Example(): JSX.Element {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="space-y-6">
+        <form onSubmit={handleSubmit} action="#" className="space-y-6">
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                     <img
